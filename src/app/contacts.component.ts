f0cd40a385eff26d7f91d0aeda7004508e4335cd
Van Marsenille Stijn <stijn.vanmarsenille@cegeka.com>
2016-10-19 12:59:23 +0200

    day 1 exercise 2+3

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
+import { CONTACT_DATA } from './data/contact-data';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['contacts.component.css']
 })
 export class ContactsAppComponent {
-  title = 'Angular 2 Master Class setup works!';
+  contacts : Contact[] = CONTACT_DATA;
 }
