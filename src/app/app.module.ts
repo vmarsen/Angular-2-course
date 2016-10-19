5f4dcef2e97ce783d67e98ed2dceecbe2fc400fc
Van Marsenille Stijn <stijn.vanmarsenille@cegeka.com>
2016-10-19 11:10:01 +0200

    day 1 exercise 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ContactsAppComponent } from './contacts.component';
+import { ContactsHeaderComponent } from './contacts-header/contacts-header.component';
 
 @NgModule({
-  declarations: [ContactsAppComponent],
+  declarations: [ContactsAppComponent, ContactsHeaderComponent],
   imports: [BrowserModule],
   bootstrap: [ContactsAppComponent]
 })
