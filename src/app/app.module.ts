465b4b1e1ef221d21e37da42291409d6f5849456
Van Marsenille Stijn <stijn.vanmarsenille@cegeka.com>
2016-10-19 17:58:10 +0200

    Exercise 8 Crud of contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ContactsAppComponent } from './contacts.component';
 import { ContactsHeaderComponent } from './contacts-header/contacts-header.component';
+import { ContactsService} from './contacts.service';
+import { RouterModule}  from '@angular/router';
+import { ContactsAppRoutes}  from './app.routes';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactsDetailComponent } from './contacts-detail/contacts-detail.component';
+import { ContactsEditorComponent } from './contacts-editor/contacts-editor.component';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ContactsAppComponent, ContactsHeaderComponent],
-  imports: [BrowserModule],
-  bootstrap: [ContactsAppComponent]
+  declarations: [ContactsAppComponent, ContactsHeaderComponent, ContactsListComponent, ContactsDetailComponent, ContactsEditorComponent],
+  imports: [BrowserModule, RouterModule.forRoot(ContactsAppRoutes), HttpModule, FormsModule],
+  bootstrap: [ContactsAppComponent],
+  providers: [ContactsService]
 })
 export class ContactsModule {
 
--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { Contact } from './models/contact';
-import { CONTACT_DATA } from './data/contact-data';
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: 'contacts.component.html',
   styleUrls: ['contacts.component.css']
 })
-export class ContactsAppComponent {
-  contacts : Contact[] = CONTACT_DATA;
+export class ContactsAppComponent  {
+
 }
